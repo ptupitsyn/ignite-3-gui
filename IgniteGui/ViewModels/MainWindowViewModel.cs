0e918c830ea2f5c24350905f061c7a3dc578896a
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-11-25 14:53:32 +0200

    Dark theme, table selection

--- IgniteGui/ViewModels/MainWindowViewModel.cs
+++ IgniteGui/ViewModels/MainWindowViewModel.cs
@@ -15,17 +15,17 @@ public sealed partial class MainWindowViewModel : ViewModelBase
 {
     private readonly SemaphoreSlim _connectionSemaphore = new(1, 1);
 
-    [ObservableProperty]
-    private string _connectionString = "localhost:10942";
+    [ObservableProperty] private string _connectionString = "localhost:10942";
 
-    [ObservableProperty]
-    private string _status = "Not connected.";
+    [ObservableProperty] private string _status = "Not connected.";
 
-    [ObservableProperty]
-    private bool _isLoading;
+    [ObservableProperty] private bool _isLoading;
 
-    [ObservableProperty]
-    private IReadOnlyList<ITable> _tables = Array.Empty<ITable>();
+    [ObservableProperty] private IReadOnlyList<ITable> _tables = Array.Empty<ITable>();
+
+    [ObservableProperty] private ITable? _selectedTable;
+
+    [ObservableProperty] private string _query = string.Empty;
 
     private IIgniteClient? _client;
 
@@ -67,6 +67,14 @@ public sealed partial class MainWindowViewModel : ViewModelBase
             IsLoading = false;
             _connectionSemaphore.Release();
             OnPropertyChanged(nameof(IsConnected));
+        }
+    }
+
+    partial void OnSelectedTableChanged(ITable? value)
+    {
+        if (value != null)
+        {
+            Query = "select * from " + value.Name;
         }
     }
 }
