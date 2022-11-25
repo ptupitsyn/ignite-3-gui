bbb621971b4c73ebcef7c752209986b6877f4b8a
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-11-25 15:37:22 +0200

    Enable native AOT - it works!

--- IgniteGui/ViewLocator.cs
+++ IgniteGui/ViewLocator.cs
@@ -1,7 +1,7 @@
-using System;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
 using IgniteGui.ViewModels;
+using IgniteGui.Views;
 
 namespace IgniteGui;
 
@@ -9,15 +9,15 @@ public class ViewLocator : IDataTemplate
 {
     public IControl Build(object data)
     {
-        var name = data.GetType().FullName!.Replace("ViewModel", "View");
-        var type = Type.GetType(name);
-
-        if (type != null)
-        {
-            return (Control)Activator.CreateInstance(type)!;
-        }
-        
-        return new TextBlock { Text = "Not Found: " + name };
+        // var name = data.GetType().FullName!.Replace("ViewModel", "View");
+        // var type = Type.GetType(name);
+        //
+        // if (type != null)
+        // {
+        //     return (Control)Activator.CreateInstance(type)!;
+        // }
+        //
+        return new MainWindow();
     }
 
     public bool Match(object data)
