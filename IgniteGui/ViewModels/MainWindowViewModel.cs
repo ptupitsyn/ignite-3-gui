eb683d5b3a19921a6b5f2cb9cce85ad11220e849
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-11-25 12:03:07 +0200

    wip ConnectionString

--- IgniteGui/ViewModels/MainWindowViewModel.cs
+++ IgniteGui/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,17 @@
 ﻿
+using ReactiveUI;
+
 namespace IgniteGui.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    private string _connectionString = "localhost:10800";
+
     public string Greeting => "Welcome to Avalonia!";
+
+    public string ConnectionString
+    {
+        get => _connectionString;
+        set => this.RaiseAndSetIfChanged(ref _connectionString, value);
+    }
 }
